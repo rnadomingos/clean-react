80ea8c72ca3bd2aa03ac74dce787bd7e804e3afb
Renan <rna.domingos@hotmail.com>
2022-09-14 13:41:37 -0300

    refactor: change imports to use model

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/method-signature-style */
-import { AccountModel } from 'domain/models/account-model'
+import { AccountModel } from '@/domain/models/account-model'
 
 export type AuthenticationParams = {
   email: string
