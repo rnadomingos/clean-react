be075d7b063077bead855c345284d1c40de8517e
renan <rna.domingos@hotmail.com>
2022-10-08 18:44:34 -0300

    test: test fix husky 2

--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker'
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>
   mockedAxios.post.mockResolvedValue({
-    data: faker.helpers.objectValue({ myProperty: 'myValue' }),
+    data: faker.helpers.objectValue({ myProperty: 'myValue1' }),
     status: faker.random.numeric()
   })
   return mockedAxios
