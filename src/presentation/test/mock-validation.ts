9eec8bc7af1ca99d8e8c867d162d0647152fbbe7
renan <rna.domingos@hotmail.com>
2022-11-13 09:52:08 -0300

    feat: ensure Login present error if password is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,13 +1,9 @@
 import { IValidation } from '@/presentation/protocols/validation'
 
-export class ValidationSpy implements IValidation {
+export class ValidationStub implements IValidation {
   errorMessage: string
-  fieldName: string
-  fieldValue: string
 
   validate (fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
     return this.errorMessage
   }
 }
