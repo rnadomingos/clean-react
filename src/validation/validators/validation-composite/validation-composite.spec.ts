299ade5ccbc1f972f9788d93c140602c360cbf56
renan <rna.domingos@hotmail.com>
2022-12-03 17:09:39 -0300

    test: ensure ValidationComposite returns falsy if there is no error

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
 
@@ -6,10 +7,10 @@ type SutTypes = {
   fieldValidationSpy: FieldValidationSpy[]
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
   const sut = new ValidationComposite(fieldValidationSpy)
   return {
@@ -20,10 +21,19 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails ', () => {
-    const { sut, fieldValidationSpy } = makeSut()
-    fieldValidationSpy[0].error = new Error('first_error_message')
-    fieldValidationSpy[1].error = new Error('second_error_message')
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('first_error_message')
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationSpy } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidationSpy[0].error = new Error(errorMessage)
+    fieldValidationSpy[1].error = new Error(faker.random.words())
+    const error = sut.validate('any_field', faker.random.word())
+    expect(error).toBe(error)
+  })
+
+  test('Should return falsy if any validation success ', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate('any_field', faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
