179aa2e66c875e10454ecac24c7b44929ecbd4ad
renan <rna.domingos@hotmail.com>
2022-12-28 19:51:01 -0300

    refactor: change link name

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -66,7 +66,7 @@ const Login: React.FC<Props> = ({ validation, authentication, saveAccessToken }:
             <Input type="email" name='email' placeholder='Digite seu e-mail' />
             <Input type="password" name='password' placeholder='Digite sua senha'/>
           <button data-testid='submit' className={Styles.submit} disabled={!!state.emailError || !!state.passwordError} type='submit'>Entrar</button>
-          <Link data-testid="signup" to="/signup" className={Styles.link}>Criar conta</Link>
+          <Link data-testid="signup-link" to="/signup" className={Styles.link}>Criar conta</Link>
           <FormStatus />
         </form>
       </Context.Provider>
