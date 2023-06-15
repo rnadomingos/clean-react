043584ae8c16d942eb5fbb20a4345ac11013ab2c
renan <rna.domingos@hotmail.com>
2023-06-14 23:45:08 -0300

    refactor: change validation factory to not use builder

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,4 +1,6 @@
+export * from './builder/validation-builder'
 export * from './email/email-validation'
 export * from './min-length/min-length-validation'
 export * from './required-field/required-field-validation'
 export * from './validation-composite/validation-composite'
+export * from './compare-fields/compare-fields-validation'
